c490fdc2e2a79403be5062be410c2ff164465822
BadPythonCoder <xsare435@gmail.com>
2023-02-27 19:38:23 +0000

    gateway tomfoolery

--- pybotic/util/gateway.py
+++ pybotic/util/gateway.py
@@ -1,4 +1,52 @@
-import websocket_client, time, json
+
+import websocket, time, json, threading, random
 
 class Gateway:
-	def __init__(self, intents, token
+	URI = "wss://gateway.discord.gg/?v=10&encoding=json"
+	tokenbuffer = None
+	interval = None
+	heartbeat = None
+	evs =  {}
+	ws = None
+	def __init__(self, intents):
+		self.intents = intents
+	def connect(self, token):
+		self.tokenbuffer = token
+		self.ws = websocket.WebSocketApp(self.URI, on_open=self.open,on_message=self.message,on_close=self.close,on_error=self.error)
+		self.ws.run_forever()
+	def heartbeatfunc(self):
+		while True:
+			self.ws.send(json.dumps({"op":1,"d":None}))
+			wait = self.interval*random.random()
+			time.sleep(wait/1000)
+	def open(self,ws):
+		pass
+	def close(self,ws, status, msg):
+		print(status, msg)
+	def message(self,ws,msg):
+		msg = json.loads(msg)
+		if msg["op"] == 10:
+			self.interval = msg["d"]["heartbeat_interval"]
+			self.heartbeat = threading.Thread(target=self.heartbeatfunc,daemon=True)
+			self.heartbeat.start()
+			ws.send(json.dumps({
+				"op": 2,
+				"d": {
+					"token": self.tokenbuffer,
+					"properties": {
+						"os": "UDN Systems",
+						"browser": "PyBotic ICEFOX AUTOMATION",
+						"device": "PyBotic"
+					},
+					"intents": self.intents
+				}
+			}))
+			del self.tokenbuffer
+		elif msg['op'] == 0:
+			if msg["t"] in self.evs:
+				self.evs[msg["t"]](msg["d"])
+	def error(self,ws,err):
+		raise err
+
+	def bind(self, eventname, eventfunc):
+		self.evs[eventname] = eventfunc
---
+++ tests/test_gateway.py
@@ -0,0 +1,15 @@
+from pybotic.util.gateway import Gateway
+
+f = open(".TOKEN")
+token = f.read()[:-1]
+f.close()
+
+def test_gateway():
+	gateway = Gateway(65277)
+	def msg(d):
+		print(d)
+		if d["content"] == "stop":
+			gateway.ws.send("GOOGOGAAGAA")
+	gateway.bind("MESSAGE_CREATE",msg)
+	gateway.connect(token)
+	assert False
